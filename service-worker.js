7940d9b29dd53a3eeb426bdca4342e800490d6a9
Ian Clark <ian@olsen-clark.com>
2017-08-28 21:57:48 -0500

    Added basic PWA files.

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function() {
+    console.log('Service Install...');
+});
+
+self.addEventListener('activate', function() {
+    console.log('Service Activate...');
+});
